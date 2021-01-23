d0768b3ba3ec00a771dd908640b9802124fe63fe
mis6us <mis6us@icloud.com>
2021-01-23 11:57:37 +0200

    Initial commit

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let elements = document.querySelectorAll("ul#items li");
+    let textArea = document.querySelector("#result");
+
+    for (let node of elements) {
+        textArea.value += node.textContent + "\n";
+    }
 }
--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,8 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstNumber = Number(document.getElementById('num1').value);
+    let secondNumber = Number(document.getElementById('num2').value);
+
+    let result = firstNumber + secondNumber;
+
+    document.getElementById('sum').value = result;
 }
